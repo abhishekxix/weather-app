46d7af6d32671cfd0cdf6902c227971752173f96
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-15 12:11:05 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+require('express-async-errors');
+const express = require('express');
+const app = express();
+
+// middleware
+app.use(express.static('./public'));
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is listening at port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
